2dafc3c4f71f78f3d93dfba246b6a6e96bfc7ee3
Tong Liu <tongyliu@gmail.com>
2018-01-28 15:37:32 -0800

    Add responsive grid

---
+++ v3/js/components/grid-pane.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GridPane = (props) => (
+  <div className="grid-pane" key={props['id']}>
+    <div className="grid-pane__info-box"></div>
+  </div>
+)
+
+export default GridPane
--- v3/js/components/work-page.js
+++ v3/js/components/work-page.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import GridPane from './grid-pane.js'
 
-const WorkPage = () => (
-  <div className="work-page">
-  </div>
-)
+const WorkPage = () => {
+  let gridComponents = []
+  for (let i = 0; i < 12; ++i) {
+    gridComponents.push(<GridPane id={i} />)
+  }
+
+  return (
+    <div className="work-page">
+      {gridComponents}
+    </div>
+  )
+}
 
 export default WorkPage
